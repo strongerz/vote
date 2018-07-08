78235a5b1d2e5a7dc72c0e7579c1449052d2f891
strongerz <github@363104>
2018-07-08 22:32:57 +0800

    OK

--- rank.py
+++ rank.py
@@ -23,7 +23,9 @@ rank_headers = {
     'Cookie': 'PHPSESSID=a52be5a3095b3ddb29c88c4a4e88b24d; Hm_lpvt_08c6f5e17c0761a968c5658ccf6ff5ad=1531046739; Hm_lvt_08c6f5e17c0761a968c5658ccf6ff5ad=1530972177,1531005903,1531008783,1531045193'
 }
 id = []
-votecount1 = ['37931', '33339', '32214', '29514', '26238', '25449', '23626', '23081', '21192', '19071']
+giftcount1 = ['1948','5840','5972','5738','1360','5225','4251','3936','1218','3']
+giftcount2 = []
+votecount1 = ['37931', '33340', '32215', '29514', '26238', '25449', '23626', '23081', '21193', '19192']
 votecount2 = []
 for a in range(999999):
     try:
@@ -35,11 +37,14 @@ for a in range(999999):
     for i in range(1, 11):
         id.append(html.split("\"id\"")[i].split("\"")[1])
         votecount2.append(html.split("\"id\"")[i].split("\"votenum\"")[1].split("\"")[1])
+        giftcount2.append(html.split("\"id\"")[i].split("\"giftcount\"")[1].split(",")[0].split(":")[1])
     for k in range(10):
-        print("第%d名ID为%s，票数为%s，增幅为%d" % (k+1, id[k], votecount2[k],int(votecount2[k])-int(votecount1[k])))
+        print("第%d名ID为%s，票数%s，礼物%s，票数增幅%d，礼物增幅%d" % (k+1, id[k], votecount2[k],giftcount2[k],int(votecount2[k])-int(votecount1[k]),int(giftcount2[k])-int(giftcount1[k])))
     votecount1=votecount2
+    giftcount1=giftcount2
     id = []
     votecount2 = []
+    giftcount2 = []
     time.sleep(3600)
 
 
